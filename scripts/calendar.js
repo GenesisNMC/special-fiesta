17d0ddc2d10a5de5e9bf2c826905e0aa0c1b623f
Genesis NMC <40467148+GenesisNMC@users.noreply.github.com>
2020-12-06 12:51:11 +0000

    Initial commit

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -6,11 +6,11 @@ function Door(calendar, day) {
 	this.width = ((calendar.width - 0.1 * calendar.width) / 4) * 0.95;
 	this.height = ((calendar.height - 0.1 * calendar.height) / 6) * 0.95;
 	this.adventMessage = 'Day ' + day + ' of Advent\n\n' + '"' + messages[day - 1][0] + '"\n\n\t' + 'by ' + messages[day - 1][1] + '\n\n';
-	this.x = ( 0.04 * calendar.width + ((day- 1) % 4) * (1.1 * this.width) );
-	this.y = - ( 0.96 * calendar.height - Math.floor((day - 1) / 4) * (1.1 * this.height) );
+	this.x = (0.04 * calendar.width + ((day - 1) % 4) * (1.1 * this.width));
+	this.y = - (0.96 * calendar.height - Math.floor((day - 1) / 4) * (1.1 * this.height));
 
-	this.content = function() { 
-		
+	this.content = function () {
+
 		var node = document.createElement("li");
 		document.getElementById("adventDoors").appendChild(node);
 		node.id = "door" + day;
@@ -21,26 +21,26 @@ function Door(calendar, day) {
 		innerNode.innerHTML = day;
 		innerNode.href = "#";
 
-		if( ( currentDate.getMonth() + 1 ) < 12 || currentDate.getDate() < day ) {
+		if ((currentDate.getMonth() + 1) < 12 || currentDate.getDate() < day) {
 			innerNode.className = "disabled";
-			innerNode.onclick = function() {
+			innerNode.onclick = function () {
 				return false;
 			}
 		} else {
 			var adventMessage = this.adventMessage;
-			innerNode.onclick = function() {
+			innerNode.onclick = function () {
 				alert(adventMessage);
 				return false;
 			}
-		}	
+		}
 	};
 
 }
 
-(function() {
+(function () {
 	var doors = [];
 
-	for(var i = 0; i < 24; i++) {
+	for (var i = 0; i < 24; i++) {
 
 		doors[i] = new Door(myCal, i + 1);
 		doors[i].content();
