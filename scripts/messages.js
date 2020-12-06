37b96ff954ab93a994f87dece33a0b108bd51833
Genesis NMC <40467148+GenesisNMC@users.noreply.github.com>
2020-12-06 16:17:08 +0000

    Revert "Update messages.js"
    This reverts commit 05d4bb120bd5c055a6ebe2c299d4dc5e182fe6df.

--- scripts/messages.js
+++ scripts/messages.js
@@ -1,53 +1,11 @@
 var messages = [
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	['Sorry I was on mute'],
-	['Could you go back one slide please?'],
-	['(swears under breath)'],
-	['Let\'s take that offline'],
-	['Can you record the call please?'],
-	['There seems to be a lot of background noise'],
-	['We\'ll put that in the parking lot'],
-	['It is what it is'],
-	['We are where we are'],
-	['Can you see my screen?'],
-	['In the interests of transparency'],
-	['Why? (x5)'],
-	['Sorry there\'s a bit of a lag'],
-	['Could you go on mute if you\'re not speaking?'],
-	['Excuse the noise, I\'m just driving/on the train/walking'],
-	['Your sound is feeble'],
-	['We can\'t hear you, are you on mute?'],
-	['Welcome to the Dream Team!'],
-	['Sorry, could you repeat the question?'],
-	['Sorry, I was responding to an email, what was the question?'],
-	['Let\'s give it a few minutes before we start'],
-	['<dog barking>'],
-	['Was that a question or a statement?'],
-	['We\'re out of time'],
-=======
-=======
->>>>>>> parent of 4d7c679... Correct typos
-=======
->>>>>>> parent of 4d7c679... Correct typos
-=======
->>>>>>> parent of 4d7c679... Correct typos
-=======
->>>>>>> parent of 4d7c679... Correct typos
-	["Safe travel to all Data Migrants and may all your Data Models be Harmonised", "Alvin Navarro, Data Lead"],
-=======
-	["Peace to all Data Migrants and may all your Data Models be Harmonised", "Alvin Navarro, Data Lead"],
->>>>>>> parent of 38d81a8... Update messages
+	["Safes travel to all Data Migrants and may all your Data Models be Harmonised", "Alvin Navarro, Data Lead"],
 	["Christmas is a necessity. There has to be at least one day of the year to remind us that we're here for something else besides ourselves.", "Lukas Litzkow, PMO"],
 	["Hope Santa Claus Procures all the gifts on your list and you Pay a good price", "Lynn Bryce, P2P Lead"],
 	["Hope you are Recorded on the Nice List, and not Reported on the Naughty List", "Ian Struggles, R2R Lead"],
 	["Hope you receive all your Christmas Orders and maybe even some Cash", "Rohn Dutta, O2C Lead"],
-	["Never worry about the size of your Christmas tree. In the eyes of children, they are all thirty feet tall.", "Thomas Leuschner, Programme Director"],
 	["Christmas magic is silent. You don't hear it --- you feel it, you know it, you believe it.", "Cesare Guarneri, Programme Manager"],
+	["Never worry about the size of your Christmas tree. In the eyes of children, they are all thirty feet tall.", "Thomas Leuschner, Programme Director"],
 	["Wishing you an Excellent Christmas, from the Centre of my heart", "Karin Ginzky, Process COE Lead"],
 	["Christmas, my child, is love in action. Every time we love, every time we give, it's Christmas.", "Tonio Vos, Young G"],
 	["What kind of Christmas present would Jesus ask Santa for?", "Deven Odedra, Technical Lead"],
@@ -65,5 +23,4 @@ var messages = [
 	["Christmas works like glue, it keeps us all sticking together.", "Florian Boersting, SNP Project Manager"],
 	["Christmas is a bridge. We need bridges as the river of time flows past. Today's Christmas should mean creating happy hours for tomorrow and reliving those of yesterday.", "Hayden Bamber, IBM Programme Director"],
 	["The real Santa Claus is at the mall.", "Lemony Snicket"],
->>>>>>> parent of 4d7c679... Correct typos
 ];
