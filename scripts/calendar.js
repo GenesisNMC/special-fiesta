21a2575629813fb71feb72361566aa1a3b91570e
Genesis NMC <40467148+GenesisNMC@users.noreply.github.com>
2020-12-07 15:55:19 +0000

    Refresh

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -21,18 +21,32 @@ function Door(calendar, day) {
 		innerNode.innerHTML = day;
 		innerNode.href = "#";
 
-		if ((currentDate.getMonth() + 1) < 12 || currentDate.getDate() < day) {
-			innerNode.className = "disabled";
-			innerNode.onclick = function () {
-				return false;
-			}
-		} else {
-			var adventMessage = this.adventMessage;
-			innerNode.onclick = function () {
-				alert(adventMessage);
-				return false;
-			}
+		// if ((currentDate.getMonth() + 1) < 12 || currentDate.getDate() < day) {
+		// 	innerNode.className = "disabled";
+		// 	innerNode.onclick = function () {
+		// 		return false;
+		// 	}
+		// } else {
+		// 	var adventMessage = this.adventMessage;
+		// 	innerNode.onclick = function () {
+		// 		alert(adventMessage);
+		// 		return false;
+		// 	}
+		// }
+
+
+		// if ((currentDate.getMonth() + 1) < 12 || currentDate.getDate() < day) {
+		// 	innerNode.className = "disabled";
+		// 	innerNode.onclick = function () {
+		// 		return false;
+		// 	}
+		// } else {
+		var adventMessage = this.adventMessage;
+		innerNode.onclick = function () {
+			alert(adventMessage);
+			return false;
 		}
+
 	};
 
 }
