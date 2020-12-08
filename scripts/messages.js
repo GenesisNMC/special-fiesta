1f2dd6fdbea3cd0f61841a35479bfa1b3ceacb9f
Genesis NMC <40467148+GenesisNMC@users.noreply.github.com>
2020-12-08 17:03:53 +0000

    Updates BW and BPC messages

--- scripts/messages.js
+++ scripts/messages.js
@@ -18,9 +18,9 @@ var messages = [
 	["Craciun Fericit!", "Romania"],
 	["The S/4EMEA journey in 2020 has been a rough ride for everyone involved. I like to take the opportunity to thank all of you personally for all the hours and hard work that you have put into it this year! As stated half a year ago, we can only deliver this together and I am looking forward to a successful delivery together with all of you in 2021! Now I wish all of you a very merry Christmas with your families, some relaxation and a fantastic start into a hopefully more normal becoming year 2021!", "Thomas Leuschner, Programme Director"],
 	["Buon Natale!", "Italy"],
-	["What part of the keyboard do astronauts like best?", "The spacebar"],
+	["Hope your toys come out perfectly from Santa's Business Warehouse", "BW Workstream"],
 	["All I want for Christmas is SAP_ALL", "Enterprise Architecture"],
-	["Why was the cell phone wearing glasses?", "It lost its contacts"],
+	["Hope your family has a fun Consolidation at dinner", "BPC Workstream"],
 	["Why did the PowerPoint Presentation cross the road?", "To get to the other slide"],
 	["Eminem walks into a bar, asks for two tequilas. The barman says...", "You only get one shot"],
 ];
